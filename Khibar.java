a70bb756c1a6d72f75ddd3512ad7d4d7631bd9a4
kibarpusaka <tuple1004@gmail.com>
2019-03-06 08:23:48 +0700

    first commit

---
+++ Khibar.java
@@ -0,0 +1,8 @@
+public class Khibar{
+	public static void(String[args]){
+	        System.out.println("KHIBAR PUSAKA");
+		System.out.println("17090104");
+ 		System.out.println("KELAS 4C");
+	}
+}
+
